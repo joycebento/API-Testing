650c8625ae0e8284e1104c27e0d3444c9d875507
joycebento <joycebentof@gmail.com>
2024-05-31 09:59:26 -0300

    Comments

--- src/test/java/test/TestSchema.java
+++ src/test/java/test/TestSchema.java
@@ -2,7 +2,6 @@ package test;
 
 import io.restassured.http.ContentType;
 import org.junit.Test;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -13,38 +12,50 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestSchema {
-    
+
+    //End point a ser testada:
     String api = "https://gorest.co.in/public/v2/todos";
 
     @Test
-    public void testSchema(){
+    public void responseSchemaValidation(){
         given()
+         // Entrada na API
         .when().get(api)
         .then().log().all()
-                .assertThat().body(matchesJsonSchemaInClasspath("schema.json"))
-                .statusCode(200).contentType(ContentType.JSON);
+
+            //Validação do Schema da response
+            .assertThat().body(matchesJsonSchemaInClasspath("schema.json"))
+
+            //Validação de status code e tipo de conteúdo da response
+            .statusCode(200).contentType(ContentType.JSON);
     }
 
     @Test
     public void validateStatusCompleted() {
+        given()
+
         //Entrada na API
-        get(api)
+        .when().get(api)
 
         //Validação de status dos usuários
         .then().log().all()
                 .assertThat().body("status",not(hasItem("pending")));
 
         /* Os objetos retornados por esta API podem ter 2 valores no campo status: pending e completed
-        Se este teste falhar, quer dizer que nem todos os objetos tem status completed */
+        Se este teste falhar, significa que nem todos os objetos tem status completed */
     }
 
     @Test
     public void validateDueOn() {
+
+        //Captura de valores do campo "due_on"
         List<String> dueOns = get(api).jsonPath().getList("due_on");
 
+        //Interpretação dos valores de "due_on" e conversão de formato
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
         List<LocalDateTime> dueDates = dueOns.stream().map(d -> LocalDateTime.parse(d, formatter)).toList();
 
+        //Verificação se todas as datas de "due_on" são posteriores à data atual
         assertThat("All dates should be after today",
            dueDates.stream().allMatch(dd -> dd.isAfter(LocalDateTime.now())),
            is(true)
